9a2d81ba5fb187885fd26ffc2d5cb720067023f3
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-08-11 14:14:04 +0530

    Create 4323-CountSubarraysWithEvenOddRatioI

---
+++ 4323-CountSubarraysWithEvenOddRatioI/4323-CountSubarraysWithEvenOddRatioI.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 2:14:04 PM
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a, int b) {
+        int ans=0;
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            int even=0;
+            int odd=0;
+
+            for(int j=i;j<n;j++){
+                if(nums[j]%2==0)
+                    even++;
+                else 
+                    odd++;
+
+                if(odd>0 && (long)even*b<=(long)odd*a)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+}
