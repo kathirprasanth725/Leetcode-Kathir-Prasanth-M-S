69cc66f0f1ecbcca460fb659cb28150dd5b7dbb0
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-09-11 09:27:52 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,9 @@
+// Last updated: 9/11/2026, 9:27:51 AM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+        return true;
+        else 
+        return false;
+    }
+}
