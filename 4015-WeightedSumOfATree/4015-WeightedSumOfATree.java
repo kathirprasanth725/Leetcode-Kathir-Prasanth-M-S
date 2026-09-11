67c3353205947e308653dd8864ec8bff447acb59
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-09-11 09:23:09 +0530

    Create 4015-WeightedSumOfATree

---
+++ 4015-WeightedSumOfATree/4015-WeightedSumOfATree.java
@@ -0,0 +1,34 @@
+// Last updated: 9/11/2026, 9:23:09 AM
+class Solution {
+    public long weightedSum(int[] parent, int[] nums) {
+        int n=parent.length;
+        ArrayList<Integer>[] children =new ArrayList[n];
+        for(int i=0;i<n;i++)
+            children[i]=new ArrayList<>();
+
+        for(int i=1;i<n;i++)
+            children[parent[i]].add(i);
+
+        int[] depth=new int[n];
+        Queue<Integer> q=new LinkedList<>();
+
+        depth[0]=1;
+        q.add(0);
+
+        int height=1;
+
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int child:children[node]){
+                depth[child]=depth[node]+1;
+                height=Math.max(height,depth[child]);
+                q.add(child);
+            }
+        }
+            long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=(long)nums[i]*(height-depth[i]+1);          
+        }
+        return sum;
+    }
+}
