4daaa412b59ddcfd75bbaf33b08cf0cb66d05a39
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-09-11 09:28:35 +0530

    Create 832-FlippingAnImage

---
+++ 832-FlippingAnImage/832-FlippingAnImage.java
@@ -0,0 +1,28 @@
+// Last updated: 9/11/2026, 9:28:35 AM
+class Solution 
+{
+    public static void flip(int[] arr) 
+    {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) 
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public int[][] flipAndInvertImage(int[][] image) 
+    {
+        int n = image.length;
+        for(int i=0;i<n;i++)
+        {   
+            flip(image[i]); // Flip
+            for(int j=0;j<n;j++)
+                image[i][j] = (image[i][j] == 1) ? 0 : 1; // Invert
+        }   
+        return image;
+    }
+}
