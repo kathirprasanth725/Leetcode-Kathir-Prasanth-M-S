2068823fbc27fdb62a005ffa8401dc1222af41ac
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-09-11 09:29:11 +0530

    Create 700-SearchInABinarySearchTree

---
+++ 700-SearchInABinarySearchTree/700-SearchInABinarySearchTree.java
@@ -0,0 +1,15 @@
+// Last updated: 9/11/2026, 9:29:11 AM
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }        
+    }
+}
