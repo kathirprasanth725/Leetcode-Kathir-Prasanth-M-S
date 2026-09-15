69b29d5602dc8d94e495f7ed493a90fd4f011dad
kathirprasanth725 <kathirprasanth725@gmail.com>
2026-09-15 20:26:45 +0530

    Create 974-SubarraySumsDivisiblebyK

---
+++ 974-SubarraySumsDivisiblebyK/version/java/974-SubarraySumsDivisiblebyK_20260915_202645.java
@@ -0,0 +1,23 @@
+// Last updated: 9/15/2026, 8:26:45 PM
+1class Solution {
+2    public int subarraysDivByK(int[] nums, int k) {
+3    HashMap<Integer,Integer> hm=new HashMap<>();
+4    hm.put(0,1);
+5    int sum=0;
+6    int count=0;
+7    for(int i=0;i<nums.length;i++){
+8         sum+=nums[i];
+9         int rem=sum%k;
+10          if(rem<0){
+11            rem+=k;
+12         }
+13
+14         if(hm.containsKey(rem)){
+15            count+=hm.get(rem);
+16         }
+17            hm.put(rem,hm.getOrDefault(rem,0)+1);
+18         
+19    } 
+20    return count;
+21    }
+22}
